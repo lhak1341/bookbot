90409c50e088c4b6de44eebcf3f5485a4ca747a8
Khoa Le <lhak1341@gmail.com>
2025-11-13 19:27:35 +0700

    Use sys to access the file (no hard coded), add Exception of FileNotFoundError

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
+import sys
 from stats import get_num_words, count_chars, sort_on
 
-def get_book_text(path):
-    with open(path) as f:
-        return f.read()
-
 def main():
-    book_path = "books/frankenstein.txt"
-    book_text = get_book_text(book_path)
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    book_path = sys.argv[1]
+
+    try:
+        with open(book_path, "r", encoding="utf-8") as f:
+            book_text = f.read()
+    except FileNotFoundError:
+        print(f"File not found: {book_path}")
+        sys.exit(1)
     num_words = get_num_words(book_text)
     count_dict = count_chars(book_text)
-    # count_dict.sort(reverse=True, key=sort_on)
 
     new_list = []
     for ch, n in count_dict.items():
